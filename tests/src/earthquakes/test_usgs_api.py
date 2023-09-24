1b8fe88c4630cc1bd0d2c1cb2a80d987ad33354f
Brad Vrabete <bvrabete@gmail.com>
2023-09-24 14:27:07 +0100

    Updated the tests

--- tests/src/earthquakes/test_usgs_api.py
+++ tests/src/earthquakes/test_usgs_api.py
@@ -2,8 +2,10 @@ import pytest
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from urllib.error import URLError
-from urllib.request import Request
 from urllib.parse import urlparse, parse_qs
+import urllib.request
+from pandas import DataFrame
+
 from earthquakes.usgs_api import build_api_url, get_earthquake_data
 
 
@@ -29,6 +31,15 @@ def invalid_parameters() -> tuple:
     return (100, -200, 30000, -1, datetime(1877, 1, 1))
 
 
+@pytest.fixture
+def csv_data():
+    data = """time,latitude,longitude,depth,mag,magType,nst,gap,dmin,rms,net,id,updated,place,type,horizontalError,depthError,magError,magNst,status,locationSource,magSource
+2023-09-08T01:06:12.975Z,36.0627,25.0264,10,4.2,mb,32,83,0.781,0.7,us,us7000ku2q,2023-09-08T11:45:08.235Z,"49 km SW of Emporeío, Greece",earthquake,5.41,1.925,0.136,15,reviewed,us,us
+2023-08-28T22:10:19.406Z,35.7724,25.4996,11.932,4.5,mb,74,76,0.693,0.61,us,us7000krp7,2023-09-12T01:10:33.926Z,"50 km N of Limín Khersonísou, Greece",earthquake,2.6,4.358,0.066,67,reviewed,us,us
+"""
+    return data.strip()
+
+
 def test_valid_parameters(valid_parameters: tuple) -> None:
     """
     Test the validity of the given parameters for the build_api_url function.
@@ -52,7 +63,7 @@ def test_valid_parameters(valid_parameters: tuple) -> None:
         min_magnitude=minimum_magnitude,
         end_date=end_date,
     )
-    assert isinstance(result, Request)
+    assert isinstance(result, urllib.request.Request)
     assert result.full_url.startswith(
         "https://earthquake.usgs.gov/fdsnws/event/1/query"
     )
@@ -100,45 +111,102 @@ def test_invalid_parameters(invalid_parameters):
     assert result is None
 
 
-# # Test cases
-# def test_get_earthquake_data_valid_request(monkeypatch, test_data):
-#     # Mock the urlopen function to return a response
-#     def mock_urlopen(request):
-#         class MockResponse:
-#             def read(self):
-#                 return b"Mock response data"
+# Test cases
 
-#             def close(self):
-#                 # This is a mocked method
-#                 pass
-
-#         return MockResponse()
-
-#     monkeypatch.setattr("urllib.request.urlopen", mock_urlopen)
-
-#     latitude, longitude, radius, minimum_magnitude, end_date = test_data
-#     # Call the function under test
-#     result = get_earthquake_data(
-#         latitude, longitude, radius, minimum_magnitude, end_date
-#     )
-
-#     # Check that the function returns a non-empty DataFrame
-#     assert not result.empty
+import earthquakes
 
 
-# def test_get_earthquake_data_invalid_request(monkeypatch):
-#     # Mock the urlopen function to raise a URLError
-#     def mock_urlopen(request):
-#         raise URLError("Mock URLError")
+def test_get_earthquake_data_valid_request(
+    monkeypatch, valid_parameters: tuple, csv_data: str
+):
+    """
+    Test the get_earthquake_data function with valid request parameters and valid CSV data.
+    Mock the urlopen function to return a response
 
-#     monkeypatch.setattr("urllib.request.urlopen", mock_urlopen)
+    Args:
+        monkeypatch: A pytest fixture that allows us to modify the behavior of
+            functions or objects during test execution.
+        valid_parameters (tuple): A tuple containing the latitude, longitude,
+            radius, minimum_magnitude, and end_date parameters for the function
+            under test.
+        csv_data (str): A string representing the CSV data that will be returned
+            by the mocked urlopen function.
 
-#     latitude, longitude, radius, minimum_magnitude, end_date = test_data
+    Returns:
+        None
+    """
 
-#     # Call the function under test
-#     result = get_earthquake_data(
-#         latitude, longitude, radius, minimum_magnitude, end_date
-#     )
+    class MockResponse:
+        def __init__(self, data):
+            self.data = data.splitlines()
+            self.index = 0
 
-#     # Check that the function returns an empty DataFrame
-#     assert result.empty
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.index >= len(self.data):
+                raise StopIteration
+            else:
+                line = self.data[self.index] + "\n"
+                self.index += 1
+                return line.encode()
+
+        def close(self):
+            # This is a mocked method
+            pass
+
+    def mock_urlopen(request):
+        """
+        A function that mocks the `urlopen` function from the `urllib.request` module used
+        in the `earthquakes.usgs_api` module.
+
+        Parameters:
+            request (object): The request object to be passed to the `urlopen` function.
+
+        Returns:
+            object: A `MockResponse` object.
+
+        """
+        return MockResponse(csv_data)
+
+    monkeypatch.setattr(earthquakes.usgs_api, "urlopen", mock_urlopen)
+
+    latitude, longitude, radius, minimum_magnitude, end_date = valid_parameters
+    # Call the function under test
+    result = get_earthquake_data(
+        latitude, longitude, radius, minimum_magnitude, end_date
+    )
+
+    # Check that the function returns a 2 line DataFrame
+    assert len(result) == 2
+
+
+def test_get_earthquake_data_invalid_request(monkeypatch, valid_parameters: tuple):
+    """
+    Test the behavior of the get_earthquake_data function when an invalid request is made.
+
+    Args:
+        monkeypatch (fixture): The monkeypatch fixture used to mock the urlopen function.
+        valid_parameters (tuple): The valid parameters for the get_earthquake_data function.
+
+    Returns:
+        None
+    """
+
+    # Mock the urlopen function to raise a URLError
+    def mock_urlopen(request):
+        raise URLError(f"Mock URLError for request: {request}")
+
+    monkeypatch.setattr(earthquakes.usgs_api, "urlopen", mock_urlopen)
+
+    # Destructure the valid parameters tuple
+    latitude, longitude, radius, minimum_magnitude, end_date = valid_parameters
+
+    # Call the function under test and assign the result to a variable
+    result = get_earthquake_data(
+        latitude, longitude, radius, minimum_magnitude, end_date
+    )
+
+    # Check that the function returns an empty DataFrame using the `empty` attribute
+    assert result.empty
